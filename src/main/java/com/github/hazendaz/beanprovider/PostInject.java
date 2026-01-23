beff3e1fd9c1dba07e01e0e70c278b0c8032d479
Jeremy Landis <jeremylandis@hotmail.com>
2026-01-22 21:21:49 -0500

    Add PostInject annotation

---
+++ src/main/java/com/github/hazendaz/beanprovider/PostInject.java
@@ -0,0 +1,25 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright 2011-2026 Hazendaz
+ */
+package com.github.hazendaz.beanprovider;
+
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * The PostInject annotation to be used with Bean Provider to post inject object. In code, this annotation would replace
+ * <code>@Inject</code> and be used via BeanProvider to cause the Injection point.
+ */
+@Target({ METHOD, CONSTRUCTOR, FIELD })
+@Retention(RUNTIME)
+@Documented
+public @interface PostInject {
+    // Post Inject Annotation
+}
